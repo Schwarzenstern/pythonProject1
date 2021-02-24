85bf81b97d5c3b289920a58f932fa7ddeefbe362
Schwarzenstern <Schwarzenstern@rambler.ru>
2021-02-24 13:07:31 +0200

    first commit

--- taskmanager/main/views.py
+++ taskmanager/main/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
-def index(request) :
+def index(request):
     return render(request, 'main/index.html')
 
 
-def about(request) :
+def about(request):
     return render(request, 'main/about.html')
